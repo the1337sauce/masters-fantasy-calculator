0b1b9597c775f87a2ff514e923b4b6123dcf765a
the1337sauce <nl@viewthespace.com>
2017-04-08 13:25:53 -0400

    Add picks

--- calculate_ranks.rb
+++ calculate_ranks.rb
@@ -25,7 +25,7 @@ def calculate_scores_for_participants
 	parsed_golfer_result_response = JSON.parse(RestClient.get 'http://samsandberg.com/themasters/', {accept: :json})['players']
 	golfer_results = parsed_golfer_result_response.map { |player| GolferResult.new(player) }
 
-	fantasy_picks = CSV.read('picks.csv')
+	fantasy_picks = CSV.read('picks_safe.csv')
 	fantasy_picks.shift
 	fantasy_participants = fantasy_picks.map { |participant_row| FantasyParticipant.new(participant_row) }
 
