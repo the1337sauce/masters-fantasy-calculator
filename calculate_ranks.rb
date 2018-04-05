5fc6ef7c2ea8b5e1f0b92002553d3b80a3072c45
the1337sauce <linsna01@gmail.com>
2018-04-05 07:52:59 -0400

    Updated picks

--- calculate_ranks.rb
+++ calculate_ranks.rb
@@ -37,9 +37,9 @@ def calculate_scores_for_participants
 	parsed_golfer_result_response = JSON.parse(RestClient.get 'http://samsandberg.com/themasters/', {accept: :json, user_agent: 'linsna01'})['players']
 	golfer_results = parsed_golfer_result_response.map { |player| GolferResult.new(player) }
 
-	fantasy_picks = CSV.read('2018_picks.csv')
+	fantasy_picks = CSV.read('2018_picks_latest.csv')
 	fantasy_picks.shift
-	
+
 	fantasy_participants = fantasy_picks.map { |participant_row| FantasyParticipant.new(participant_row) }
 
 	scorer = FantasyScorer.new(fantasy_participants, golfer_results)
